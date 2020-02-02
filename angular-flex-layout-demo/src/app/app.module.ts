dd22d16b6820216ed2927e346cca727964d75c65
MounderBoutebba <mounderboutebba@outlook.fr>
2020-02-02 20:14:38 +0100

    playing with flex

---
+++ angular-flex-layout-demo/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule, MatCardModule } from "@angular/material";
+
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
